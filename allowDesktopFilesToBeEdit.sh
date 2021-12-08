a1f56a7e0bbdc6afbdfa24aa45d5a3abe44748ec
Jacob Rush <jhrush@unomaha.edu>
2021-12-08 10:49:16 -0600

    Quick Commit

---
+++ allowDesktopFilesToBeEdit.sh
@@ -0,0 +1,8 @@
+desktopFiles=($(ls files/))
+fileNum=${!desktopFiles[@]}
+#echo "${desktopFiles[1]}"
+
+for i in $fileNum;
+do
+    sudo chattr -i "/usr/share/applications/${desktopFiles[$i]}"
+done
