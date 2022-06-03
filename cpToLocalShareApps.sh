13174bc9e67c0e163fec42f29967338163118f0b
jhrush <jhrush53@gmail.com>
2022-06-03 11:22:35 -0500

    Quick Commit

--- cpToLocalShareApps.sh
+++ cpToLocalShareApps.sh
@@ -1,4 +1,4 @@
 # Desktop files in this directory should override
 # the ones in /usr/share/applications/
 sudo cp files/* ~/.local/share/applications/
-sudo update-desktop-database ~/.local/share/applications/
+update-desktop-database ~/.local/share/applications/
